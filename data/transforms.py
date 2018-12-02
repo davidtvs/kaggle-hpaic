38d99dcb8f3f5f9c595a7f06e712a0d11b0a5400
David Silva <davidtvs10@gmail.com>
2018-12-02 16:05:21 +0000

    Image augmentation

--- data/transforms.py
+++ data/transforms.py
@@ -1,17 +1,29 @@
 import torch
-import torchvision.transforms.functional as F
+import torchvision.transforms as transform
 
 
 def to_tensor(input, target):
-    input = F.to_tensor(input)
+    input = transform.ToTensor()(input)
     target = torch.tensor(target, dtype=torch.float)
     return input, target
 
 
 class Augmentation(object):
-    def __init__(self, size):
-        self.size = size
+    def __init__(
+        self, size, degrees=20, brightness=0.25, contrast=0.25, saturation=0.25
+    ):
+        self.image_aug = transform.Compose(
+            [
+                transform.Resize(size),
+                transform.RandomHorizontalFlip(),
+                transform.RandomVerticalFlip(),
+                transform.RandomRotation(degrees),
+                transform.ColorJitter(
+                    brightness=brightness, contrast=contrast, saturation=saturation
+                ),
+            ]
+        )
 
     def __call__(self, input, target):
-        input = F.resize(input, self.size)
+        input = self.image_aug(input)
         return to_tensor(input, target)
