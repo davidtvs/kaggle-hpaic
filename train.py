d40defa2abd461cd381c1f5184c38ced2e21058a
David Silva <davidtvs10@gmail.com>
2018-12-07 17:56:10 +0000

    Add learning rate finder

--- train.py
+++ train.py
@@ -20,8 +20,8 @@ def arguments():
         "--config",
         "-c",
         type=str,
-        default="config/example_train.json",
-        help="Path to the JSON configuration file. Default: config/example_train.json",
+        default="config/example_kfold.json",
+        help="Path to the JSON configuration file. Default: config/example_kfold.json",
     )
 
     return parser.parse_args()
@@ -59,32 +59,10 @@ def sigmoid_threshold(tensor, threshold=0.5, high=1, low=0):
     return torch.where(out > threshold, high, low)
 
 
-if __name__ == "__main__":
-    random_state = 92
-    num_classes = 28
-
-    # Get script arguments and JSON configuration
-    args = arguments()
-    config = utils.load_config(args.config)
-
-    # Initialize the dataset and get the K-fold dataloaders
-    image_size = (config["img_h"], config["img_w"])
-    dataset = data.HPADataset(
-        config["dataset_dir"],
-        config["image_mode"],
-        transform=tf.Augmentation(image_size),
-        subset=config["subset"],
-        random_state=92,
-    )
-    ksets = data.utils.kfold_split(dataset, config["n_splits"], random_state=92)
-    dataloaders = data.utils.kfold_loader(
-        ksets, config["batch_size"], num_workers=config["workers"]
-    )
-
-    # Compute class weights
-    if config["weighing"]:
-        weighing_name = config["weighing"].lower()
-        labels = dataset.targets
+def get_weights(weighing_name, labels, device):
+    """Computes class weights given the name of the weighing technique."""
+    if weighing_name:
+        weighing_name = weighing_name.lower()
         if weighing_name == "fb":
             weights = data.utils.frequency_balancing(labels)
         elif weighing_name == "median_fb":
@@ -98,7 +76,49 @@ if __name__ == "__main__":
     else:
         weights = np.ones((num_classes,))
 
-    weights = torch.tensor(weights, dtype=torch.float, device=config["device"])
+    weights = torch.tensor(weights, dtype=torch.float, device=device)
+
+    return weights
+
+
+def get_metric_list(dataset):
+    """Initialize metrics
+
+    The main metric is the macro F1 score. Additionally, the F1 score for each class,
+    and the overall accuracy are also computed.
+    """
+    metrics = [metric.FBetaScore(name="f1_macro")]
+    for label in sorted(dataset.label_to_name):
+        name = "f1_" + dataset.label_to_name[label]
+        metrics.append(metric.FBetaScore(labels=label, name=name))
+
+    metrics.append(metric.Accuracy())
+
+    return metric.MetricList(metrics)
+
+
+if __name__ == "__main__":
+    # Get script arguments and JSON configuration
+    args = arguments()
+    config = utils.load_config(args.config)
+
+    # Configs that are used multiple times
+    device = torch.device(config["device"])
+    random_state = config["random_state"]
+
+    # Initialize the dataset and get the K-fold dataloaders
+    image_size = (config["img_h"], config["img_w"])
+    dataset = data.HPADataset(
+        config["dataset_dir"],
+        config["image_mode"],
+        transform=tf.Augmentation(image_size),
+        subset=config["subset"],
+        random_state=random_state,
+    )
+    num_classes = len(dataset.label_to_name)
+
+    # Compute class weights
+    weights = get_weights(config["weighing"], dataset.targets, device)
     print(weights)
 
     # Initialize the model
@@ -118,35 +138,63 @@ if __name__ == "__main__":
     else:
         raise ValueError("invalid loss: {}".format(config["loss"]))
 
-    # Optimizer
-    optimizer = optim.Adam(net.parameters(), lr=config["lr_rate"])
+    # The standard stuff is all done, now lets handle the different operation modes
+    mode_name = config["mode"].lower()
+    if mode_name == "find_lr":
+        # Run the learning rate finder
+        # Split the dataset intro training and validation
+        train_set, val_set = data.utils.train_val_split(
+            dataset, val_size=config["val_size"], random_state=random_state
+        )
+        train_loader, _ = data.utils.train_val_loader(
+            train_set, val_set, config["batch_size"], num_workers=config["workers"]
+        )
 
-    # Initialize metrics: the main metric is the macro F1 score; additionally, we'll
-    # compute the F1 score for each class, and the accuracy
-    metrics = [metric.FBetaScore(name="f1_macro")]
-    for label in range(num_classes):
-        name = "f1_" + dataset.label_to_name[label]
-        metrics.append(metric.FBetaScore(labels=label, name=name))
-    metrics.append(metric.Accuracy())
-    metrics = metric.MetricList(metrics)
+        # Optimizer
+        optimizer = optim.Adam(net.parameters(), lr=config["initial_lr"])
+        lr_finder = utils.LRFinder(net, optimizer, criterion, device=device)
+        lr_finder.range_test(
+            train_loader,
+            end_lr=config["end_lr"],
+            num_iter=config["num_iter"],
+            step_mode=config["step_mode"],
+        )
+        lr_finder.plot()
+    elif mode_name == "kfold":
+        # K-fold training
+        # Split dataset into k-sets and get one dataloader for each set
+        ksets = data.utils.kfold_split(
+            dataset, config["n_splits"], random_state=random_state
+        )
+        dataloaders = data.utils.kfold_loader(
+            ksets, config["batch_size"], num_workers=config["workers"]
+        )
 
-    # Location where the model checkpoints will be saved
-    checkpoint_dir = os.path.join(config["checkpoint_dir"], config["name"])
+        # Optimizer
+        optimizer = optim.Adam(net.parameters(), lr=config["lr_rate"])
 
-    trainer = core.KFoldTrainer(
-        net,
-        config["epochs"],
-        optimizer,
-        criterion,
-        metrics,
-        checkpoint_dir=checkpoint_dir,
-        mode="max",
-        stop_patience=config["stop_patience"],
-        lr_patience=config["lr_patience"],
-        lr_factor=config["lr_factor"],
-        min_lr=config["min_lr"],
-        device=config["device"],
-    )
-    if config["resume"] and os.path.isdir(config["resume"]):
-        trainer.resume(config["resume"])
-    trainer.fit(dataloaders, output_fn=sigmoid_threshold)
+        # Get list of metrics
+        metrics = get_metric_list(dataset)
+
+        # Location where the model checkpoints will be saved
+        checkpoint_dir = os.path.join(config["checkpoint_dir"], config["name"])
+
+        trainer = core.KFoldTrainer(
+            net,
+            config["epochs"],
+            optimizer,
+            criterion,
+            metrics,
+            checkpoint_dir=checkpoint_dir,
+            mode="max",
+            stop_patience=config["stop_patience"],
+            lr_patience=config["lr_patience"],
+            lr_factor=config["lr_factor"],
+            min_lr=config["min_lr"],
+            device=device,
+        )
+        if config["resume"] and os.path.isdir(config["resume"]):
+            trainer.resume(config["resume"])
+        trainer.fit(dataloaders, output_fn=sigmoid_threshold)
+    else:
+        raise ValueError("invalid mode in configuration file")
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .utils import *
+from .lr_finder import LRFinder
