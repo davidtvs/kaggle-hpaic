9dd7425fed5239942cc3a68cca61bed50b798b34
David Silva <davidtvs10@gmail.com>
2018-12-14 22:48:13 +0000

    Add CyclicLR implementation
    Slightly modified version of pull request #2016 from the pytorch repo: https://github.com/pytorch/pytorch/pull/2016
    The implementation follows the paper "Cyclical Learning Rates for Training Neural Networks": https://arxiv.org/abs/1506.01186

--- core/__init__.py
+++ core/__init__.py
@@ -4,3 +4,4 @@ from .trainer import Trainer, KFoldTrainer
 from .predict import predict, predict_batch, predict_yield_batch
 from .evaluate import evaluate
 from .lr_finder import LRFinder
+from .cyclic_lr import CyclicLR
